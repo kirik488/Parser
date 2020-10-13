78c88f40d081a8c818a0a7f15fdc02b497da762a
Your Name <you@example.com>
2020-10-13 11:55:11 +0300

    v1

---
+++ image.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+image_number = 0
+storage_number = 1
+link = f"https://zastavok.net"
+# Названия прогона
+run = "ц"
+# Кол-во страниц для парсинга
+for storage in range(2):
+    responce = requests.get(f"{link}/{storage_number}").text
+    soup = BeautifulSoup(responce, "lxml")
+    block = soup.find("div", "block-photo")
+    all_image = block.find_all("div", "short_full")
+
+    for image in all_image:
+        image_link = image.find("a").get("href")
+        download = requests.get(f"{link}{image_link}").text
+        download_soup = BeautifulSoup(download, "lxml")
+        download_block = download_soup.find("div","image_data").find("div", "block_down")
+        result_link = download_block.find("a").get("href")
+
+        image_bytes = requests.get(f"{link}{result_link}").content
+
+        with open(f"image/{run}{image_number}.jpg","wb") as file:
+            file.write(image_bytes)
+
+        print(f"Изображение номер:{image_number} скачено")
+        image_number += 1
+    storage_number += 1
---
+++ now.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Traffic-weather-News
+link = "https://yandex.ru/"
+recponce = requests.get(link).text
+soup = BeautifulSoup(recponce, "lxml")
+
+# Weather
+weather = soup.find("div", "widget__content weather__content-outer")
+weather_now = weather.find("a").get("aria-label")
+
+# night-evening
+weather_all = weather.find("div", "weather__forecast").text
+
+# Traffic
+traffic = soup.find("div", "traffic__content widget__content")
+traffic_number = traffic.find("div", "traffic__rate-text").text
+traffic_now = traffic.find("div", "traffic__forecast")
+traffic_text = traffic_now.find("a").text
+
+# News
+new = soup.find("ol", "list news__list")
+news_block = new.find_all("span", "news__item-content")
+
+with open("Now.txt", "w") as file:
+    file.write(f"Сейчас:{weather_now},{weather_all}\n"
+               f"Пробки:{traffic_number} балла "
+               f"{traffic_text}\n")
+    file.write("Новости:\n")
+    x = 1
+    for news in news_block:
+        a = str(news).rstrip("</span>").lstrip('<span class="news__item-content">')
+        file.write(f"{x}) {a}\n")
+        x += 1
+
+print("Готово")
