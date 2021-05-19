b1521526fd6cb6a3ad6baeee6ce11a8ef3017b0e
Your Name <you@example.com>
2021-05-19 10:21:27 +0300

    Information about weather, news, traffic jams. from yandex.ru .v5

--- now.py
+++ now.py
@@ -12,13 +12,11 @@ weather = soup.find("div", "widget__content weather__content-outer")
 weather_now = weather.find("a").get("aria-label")
 
 # night-evening
-weather_all = weather.find("div", "weather__forecast").text
+weather_ev_ni = weather.find("div", "weather__forecast").text
 
 # Traffic
 traffic = soup.find("div", "traffic__content widget__content")
 traffic_number = traffic.find("div", "traffic__rate-text").text
-traffic_now = traffic.find("div", "traffic__forecast")
-traffic_text = traffic_now.find("a").text
 
 # News
 new = soup.find("ol", "list news__list")
@@ -27,14 +25,12 @@ news_block = new.find_all("span", "news__item-content")
 time_now = time.ctime()
 with open("Now.txt", "w") as file:
     file.write(f"{time_now} \n")
-    file.write(f"Сейчас:{weather_now},{weather_all}\n"
-               f"Пробки:{traffic_number} балла "
-               f"{traffic_text}\n")
+    file.write(f"Сейчас:{weather_now}\n{weather_ev_ni}\n")
+    file.write(f"Пробки:{traffic_number} балла\n")
     file.write("Новости:\n")
     x = 1
     for news in news_block:
         a = str(news).rstrip("</span>").lstrip('<span class="news__item-content">')
         file.write(f"{x}) {a}\n")
         x += 1
-
 print(f"Completed!", time_now)
